955bf0329a00c22848733daa4ddd217944f77b8b
Cho <YXC5125@alixarx.com>
2019-04-19 18:23:02 -0500

    checking cheerio scrapper and mongoDB

---
+++ server.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require("express");
+const mongoose = require('mongoose');
+const request = require("request");
+const cheerio = require("cheerio");
+const bodyParser = require('body-parser');
+
+
+var PORT = process.env.PORT || 3030;
+
+const app = express();
+
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
+app.use(bodyParser.text({ type: 'text/html' }));
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/FootballScraper";
+
+// mongoose.connect(MONGODB_URI);
+mongoose.set('useCreateIndex', true)
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT);
+  });
